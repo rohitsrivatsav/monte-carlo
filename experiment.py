7af32e17219931e7a2f734f71cd83018b913721d
Chyld Medford <chyld.medford@gmail.com>
2019-02-14 09:57:29 -0800

    more prob

--- experiment.py
+++ experiment.py
@@ -6,3 +6,13 @@ class Experiment:
         # return the probability of head
         c = Coin()
         return sum((c.flip() for _ in range(x))) / x
+
+    def flip_coin_2x_n_times(self, n):
+        c = Coin()
+        experiments = [sum([c.flip(), c.flip()]) == 2 for _ in range(n)]
+        return sum(experiments) / n
+
+    def flip_coin_with_pattern_n_times(self, pattern, n, flips):
+        c = Coin()
+        experiments = [[c.flip() for f in range(flips)] == pattern for e in range(n)]
+        return sum(experiments) / n
--- tests/test_experiment.py
+++ tests/test_experiment.py
@@ -6,3 +6,15 @@ def test_probability_of_a_head():
     e = Experiment()
     p = e.flip_coin_x_times(100_000)
     assert p == approx(0.5, rel=0.01)
+
+
+def test_probability_of_hh():
+    e = Experiment()
+    p = e.flip_coin_2x_n_times(100_000)
+    assert p == approx(0.25, rel=0.01)
+
+
+def test_probability_of_ththh():
+    e = Experiment()
+    p = e.flip_coin_with_pattern_n_times([0, 1, 0, 1, 1], 1_000_000, 5)
+    assert p == approx((0.5)**5, rel=0.01)
